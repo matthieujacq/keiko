002a989655794be48d03fc25ef734d456a12d41f
Matthieu Jacq <matthieuj@theodo.fr>
2023-09-13 12:54:36 +0200

    :recycle: Extract Pokemon details into a component

---
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,9 @@
+export const Pokemon = () => {
+  return (
+    <div>
+      <img alt="carapuce" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" />
+      <p>Name: Carapuce</p>
+      <p>Number: 7</p>
+    </div>
+  )
+}
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Pokemon } from "components/Pokemon"
 import styles from "./Home.module.css"
 
 export const Home = () => {
@@ -5,11 +6,7 @@ export const Home = () => {
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <div>
-        <img alt="carapuce" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" />
-      </div>
-      <p>Name: Carapuce</p>
-      <p>Number: 7</p>
+      <Pokemon />
     </div>
   )
 }
