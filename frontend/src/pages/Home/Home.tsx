847a06cbaa6303f9da4662518d8df5a361431d8c
Matthieu Jacq <matthieuj@theodo.fr>
2023-09-13 15:54:52 +0200

    :poop: Naive (and not working) filter implementation

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -8,10 +8,21 @@ const pokemons = [
 ]
 
 export const Home = () => {
+  let pokemonFilterValue = ""
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    pokemonFilterValue = event.target.value
+    console.log(event.target.value)
+  }
+
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <label htmlFor="search">Rechercher un pokemon: </label>
+        <input id="search" type="text" className={styles.input} onChange={onInputChange} value={pokemonFilterValue} />
+      </div>
       {pokemons.map(({ name, id }) => (
         <Pokemon name={name} id={id} key={id} />
       ))}
