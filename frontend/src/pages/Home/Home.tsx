332985e165cab21540c6792bc94392e432b1cfd8
Matthieu Jacq <matthieuj@theodo.fr>
2023-09-13 16:57:08 +0200

    :recycle: Filter pokemons with a hook

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,45 +1,39 @@
-import { Pokemon } from "components/Pokemon"
 import styles from "./Home.module.css"
-import React from "react"
+import { Pokemon } from "components/Pokemon"
+import { useState } from "react"
 
-const pokemons = [
+interface Pokemon {
+  name: string
+  id: number
+}
+const pokemons: Pokemon[] = [
   { name: "Carapuce", id: 7 },
   { name: "Carabaffe", id: 8 },
   { name: "Tortank", id: 9 },
 ]
 
-export class Home extends React.Component {
-  state = {
-    pokemonFilterValue: "",
+function filterByName(pokemons: Pokemon[], filterValue: string) {
+  return pokemons.filter(({ name }) => name.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase()))
+}
+
+export const Home = () => {
+  const [filterValue, setFilterValue] = useState("")
+
+  const onFilterUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(event.target.value)
   }
 
-  onFilterUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ pokemonFilterValue: event.target.value })
-    // console.log(this.state.pokemonFilterValue) // not up to date because set state is async
-    // console.log(event.target.value)
-  }
-
-  render() {
-    return (
-      <div className={styles.intro}>
-        <div>Bienvenue sur ton futur pokédex !</div>
-        <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-        <div>
-          <label htmlFor="search">Rechercher un pokemon: </label>
-          <input
-            id="search"
-            type="text"
-            className={styles.input}
-            onChange={this.onFilterUpdate}
-            value={this.state.pokemonFilterValue}
-          />
-        </div>
-        {pokemons
-          .filter(({ name }) => name.toLocaleLowerCase().includes(this.state.pokemonFilterValue.toLocaleLowerCase()))
-          .map(({ name, id }) => (
-            <Pokemon name={name} id={id} key={id} />
-          ))}
+  return (
+    <div className={styles.intro}>
+      <div>Bienvenue sur ton futur pokédex !</div>
+      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <label htmlFor="search">Rechercher un pokemon: </label>
+        <input id="search" type="text" className={styles.input} onChange={onFilterUpdate} value={filterValue} />
       </div>
-    )
-  }
+      {filterByName(pokemons, filterValue).map(({ name, id }) => (
+        <Pokemon name={name} id={id} key={id} />
+      ))}
+    </div>
+  )
 }
