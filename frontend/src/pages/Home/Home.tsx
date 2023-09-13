7f787079732d62bbfcd6bd513de706593f99b9c7
Matthieu Jacq <matthieuj@theodo.fr>
2023-09-13 14:45:49 +0200

    :recycle: Iterate a list to render pokemons

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,20 @@
 import { Pokemon } from "components/Pokemon"
 import styles from "./Home.module.css"
 
+const pokemons = [
+  { name: "Carapuce", id: 7 },
+  { name: "Carabaffe", id: 8 },
+  { name: "Tortank", id: 9 },
+]
+
 export const Home = () => {
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <Pokemon name="Carapuce" id={7} />
-      <Pokemon name="Carabaffe" id={8} />
-      <Pokemon name="Tortank" id={9} />
+      {pokemons.map(({ name, id }) => (
+        <Pokemon name={name} id={id} key={id} />
+      ))}
     </div>
   )
 }
