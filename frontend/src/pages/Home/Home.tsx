fc2dd3adb4b477b97ef9d903d5b7684e62160a47
Matthieu Jacq <matthieuj@theodo.fr>
2023-09-13 16:41:11 +0200

    :poop: implement filter with a class based component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Pokemon } from "components/Pokemon"
 import styles from "./Home.module.css"
+import React from "react"
 
 const pokemons = [
   { name: "Carapuce", id: 7 },
@@ -7,25 +8,38 @@ const pokemons = [
   { name: "Tortank", id: 9 },
 ]
 
-export const Home = () => {
-  let pokemonFilterValue = ""
-
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    pokemonFilterValue = event.target.value
-    console.log(event.target.value)
+export class Home extends React.Component {
+  state = {
+    pokemonFilterValue: "",
   }
 
-  return (
-    <div className={styles.intro}>
-      <div>Bienvenue sur ton futur pokédex !</div>
-      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <div>
-        <label htmlFor="search">Rechercher un pokemon: </label>
-        <input id="search" type="text" className={styles.input} onChange={onInputChange} value={pokemonFilterValue} />
+  onFilterUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ pokemonFilterValue: event.target.value })
+    // console.log(this.state.pokemonFilterValue) // not up to date because set state is async
+    // console.log(event.target.value)
+  }
+
+  render() {
+    return (
+      <div className={styles.intro}>
+        <div>Bienvenue sur ton futur pokédex !</div>
+        <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+        <div>
+          <label htmlFor="search">Rechercher un pokemon: </label>
+          <input
+            id="search"
+            type="text"
+            className={styles.input}
+            onChange={this.onFilterUpdate}
+            value={this.state.pokemonFilterValue}
+          />
+        </div>
+        {pokemons
+          .filter(({ name }) => name.toLocaleLowerCase().includes(this.state.pokemonFilterValue.toLocaleLowerCase()))
+          .map(({ name, id }) => (
+            <Pokemon name={name} id={id} key={id} />
+          ))}
       </div>
-      {pokemons.map(({ name, id }) => (
-        <Pokemon name={name} id={id} key={id} />
-      ))}
-    </div>
-  )
+    )
+  }
 }
