49257a1e5dbe1d26bcf75c9de2fa657869d46deb
Matthieu Jacq <matthieuj@theodo.fr>
2023-09-13 11:49:00 +0200

    Remove comment

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -4,8 +4,6 @@ import { Home } from "./Home"
 describe("<Home />", () => {
   it("should display Carapuce", () => {
     render(<Home />)
-    // QUESTION: screen.getByText("Carapuce") as asked in the keiko is not found.
-    // Is the error in the keiko or in my code?
     const carapuce = screen.getByText("Name: Carapuce")
     expect(carapuce).toBeInTheDocument()
   })
