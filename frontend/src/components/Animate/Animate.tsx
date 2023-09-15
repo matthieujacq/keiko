136b7a9c47f8816224bd295b60cd4f0d0e54371e
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-09-15 11:33:51 +0200

    :sparkles: HOC Tada Animation

--- frontend/src/components/Animate/Animate.tsx
+++ frontend/src/components/Animate/Animate.tsx
@@ -1,1 +1,20 @@
-export const Animate = () => <></>
+import React from "react"
+import styles from "./Animate.module.css"
+
+export const Animate = <P extends object>(WrappedComponent: React.ComponentType<P>) => {
+  const AnimatedComponent = (props: P) => {
+    const [isAnimated, setIsAnimated] = React.useState<boolean>(false)
+
+    return (
+      <div
+        onMouseEnter={() => setIsAnimated(true)}
+        onMouseLeave={() => setIsAnimated(false)}
+        className={isAnimated ? styles.tadaAnimation : ""}
+      >
+        <WrappedComponent {...props} />
+      </div>
+    )
+  }
+  AnimatedComponent.displayName = `Animate(${WrappedComponent.displayName || WrappedComponent.name || "Component"})`
+  return AnimatedComponent
+}
